8d1f82491c886321bce2404d1f8adec7fdc5ca5b
jansaklak <101001672+jansaklak@users.noreply.github.com>
2024-03-24 20:15:50 +0100

    Update Times.h

--- Times.h
+++ Times.h
@@ -13,17 +13,19 @@
 class Times {
 private:
     int graph_size;
-    std::vector<Hardware> hw_vec;
+    std::vector<Hardware> HW_vec;
     std::vector<std::vector<int>> times_matrix;
     std::vector<std::vector<int>> cost_matrix;
     void CountCosts();
 public:
     Times();
-    Times(int graph_size, std::vector<Hardware> v);
+    Times(int graph_size, std::vector<Hardware> _HW_vec);
     void SetRandomTimesAndCosts();
-    void setTimesMatrix(std::vector<std::vector<int>> times);
-    void setCostsMatrix(std::vector<std::vector<int>> costs);
+    void setTimesMatrix(std::vector<std::vector<int>> _times_matrix);
+    void setCostsMatrix(std::vector<std::vector<int>> _costs_matrix);
     void show(std::ostream& out = std::cout);
+    int getTime(int TaskID,int HW_ID);
+    int getCost(int TaskID,int HW_ID);
     void printTimes(std::ostream& out = std::cout);
     void printCosts(std::ostream& out = std::cout);
 };
