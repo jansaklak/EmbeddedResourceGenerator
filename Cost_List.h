30e37aff4a9f764e124914ce9d0b1a6d5254b7a6
jansaklak <101001672+jansaklak@users.noreply.github.com>
2024-03-24 20:13:30 +0100

    Update Cost_List.h

--- Cost_List.h
+++ Cost_List.h
@@ -1,30 +1,36 @@
 #pragma once
 #ifndef COST_LIST_H
 #define COST_LIST_H
-
+#include<mutex>
 #include <iostream>
 #include <fstream>
 #include <set>
 #include <vector>
+#include <queue>
 #include "COM.h"
 #include "Graf.h"
 #include "Hardware.h"
 #include "Times.h"
-
+#include <stack>
+#include "TimeAndCost.h"
+#include <functional>
 class Cost_List{
     private:
+        std::vector<Hardware> Hardwares;
+        std::vector<COM> Channels;
         Graf TaskGraph;
-        std::vector<Hardware> HW_vec;
-        std::vector<COM> ch_vec;
-        Times t;
+        Times times;
         int tasks_amount;
-        int hc_amount;
-        int pu_amount;
-        int ch_amount;
+        int hardware_cores_amount;
+        int processing_unit_amount;
+        int channels_amount;
         bool with_cost;
-        bool toScreen;
+        std::vector<int> progress;
+        std::vector<std::set<int>> HWtoTasks;
+        void TaskRunner(int HWID);
     public:
-        Cost_List(int tasks,int hcores,int pus,int channels,int withcost,int toscreen);
+        
+        Cost_List(int _tasks,int _hcores,int _punits,int _channels,int _withCost);
         Cost_List();
         void CreateRandomTasksGraph();
         void printTasks(std::ostream& out = std::cout);
@@ -33,8 +39,17 @@ class Cost_List{
         void ConnectRandomCH();
         void getRandomProc();
         void RandALL();
-        void PrintALL(std::string filename);
+        void RunTasks();
+        void TaskProgress(int task_id, int time, int hw_id);
+        void ReadProgress();
+        void TaskDistribution(int rule);
+        void PrintALL(std::string filename,bool toScreen);
+        
         void Load_From_File(const std::string& filename);
+        std::vector<Hardware> getHardwares();
+        std::vector<COM> getCOMS();
+        Graf getGraph();
+        Times getTimes();
 };
 
 #endif // COST_LIST_H
