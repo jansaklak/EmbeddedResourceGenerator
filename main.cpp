1df9e8c24ffdf716512ae0484684554f278c67a7
jansaklak <101001672+jansaklak@users.noreply.github.com>
2024-03-24 20:14:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,46 +14,62 @@
 
 extern const int SCALE = 400;
 
-Cost_List losowo(){
-    
-    int tasks, hc, pu, costs, ch;
-    bool toScreen;
+Cost_List generateRandomCostList(){
+    int tasks_amount, hardware_cores_amount, processing_units_amount,channels_amount;
+    int to_screen,with_cost;
 
     std::cout << "Podaj liczbe zadan: ";
-    std::cin >> tasks;
+    std::cin >> tasks_amount;
     std::cout << "Podaj liczbe Hardware Cores: ";
-    std::cin >> hc;
+    std::cin >> hardware_cores_amount;
     std::cout << "Podaj liczbe Processing Units: ";
-    std::cin >> pu;
-    std::cout << "Czy z kosztem? 0/1: ";
-    std::cin >> costs;
+    std::cin >> processing_units_amount;
     std::cout << "Podaj liczbe szyn danych: ";
-    std::cin >> ch;
-    std::cout << "Czy wyswietic na ekranie? (0/1): ";
-    std::cin >> toScreen;
-    Cost_List lista_kosztow = Cost_List(tasks,hc,pu,ch,costs,toScreen);
+    std::cin >> channels_amount;
+    std::cout << "Czy z kosztem? 0/1: ";
+    std::cin >> with_cost;
+    
+    Cost_List lista_kosztow = Cost_List(tasks_amount,hardware_cores_amount,processing_units_amount,channels_amount,with_cost);
     lista_kosztow.RandALL();
-    lista_kosztow.PrintALL("output.dat");
     return lista_kosztow;
 }
 
-
-
 int main(){
-    int menu;
+    int menu,to_screen;
+    int running = 1;
     srand (time(NULL));
-    std::cout << "Aby stworzyć losową tabelę wcisnij 0: ";
-    std::cout << "Aby wczytać z pliku wcisnij 1: ";
-    std::cin >> menu;
-    if(menu == 0){
-        Cost_List lista = losowo();
+    std::cout << "Czy wypisywać wyniki na ekran? 0/1: ";
+    std::cin >> to_screen;
+    while(running){
+        std::cout << "Aby stworzyć losową tabelę wcisnij 0: \nAby wczytać z pliku wcisnij 1: \n Wcisnij 2 aby uruchomić zadania na liscie\nWcisnij 9 aby zakonczyc";
+        std::cin >> menu;
+        
+        if(menu == 0){
+            Cost_List lista = generateRandomCostList();
+            lista.PrintALL("output.dat",to_screen);
+            
+            
+        }
+        else if(menu == 1){
+            Cost_List lista1;
+            lista1.Load_From_File("output.dat");
+            lista1.PrintALL("input_test.dat",to_screen);
+            int strategy;
+            std::cout << "Wybierz podział zadań:\n 1-najszybciej 2-najtaniej 3-rownomiernie ";
+            std::cin >> strategy;
+            lista1.TaskDistribution(strategy);
+            lista1.RunTasks();
+            // currList = lista;
+        }
+        else if(menu == 2){
+            
+            
+            
+        }
+        else if(menu == 9){
+            running = 0;
+        }
     }
-    if(menu == 1){
-        Cost_List lista;
-        lista.Load_From_File("input.dat");
-        lista.PrintALL("input_test.dat");
-    }
-
-    std::cout << "Zapisano do pliku tasks.dat\n";
-
+    return 0;
+    
 }
