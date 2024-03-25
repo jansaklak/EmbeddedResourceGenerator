91a4c294b448e6bcfeb8baa4557553f1e41f36b2
jansaklak <101001672+jansaklak@users.noreply.github.com>
2024-03-25 17:26:22 +0100

    Update Graf.h

--- Graf.h
+++ Graf.h
@@ -1,37 +1,25 @@
 #pragma once
 
-
 #include <iostream>
-#include <vector>
 #include <fstream>
 #include <string>
-#include <sstream>
+#include <vector>
+#include "Edge.h"
 
-const int SIZE = 1001;
-class Graf
-{
-    int matrix[SIZE][SIZE];
-    int numberOfVertices;
+class Graf {
+private:
     int maxVert;
+    std::vector<std::vector<Edge>> adjList;
 public:
     Graf();
     ~Graf();
-    void createVertices(int ile);
-    std::vector<int> dijkstra(int startVertex, int endVertex) const;
     void addEdge(int i_Vertex_Index_1, int i_Vertex_Index_2);
+    void addEdge(int i_Vertex_Index_1, int i_Vertex_Index_2, int weight);
     int getWeightEdge(int i_Vertex_Index_1, int i_Vertex_Index_2) const;
-    void addEdge(int i_Vertex_Index_1, int i_Vertex_Index_2,int waga);
-    void removeEdge(int i_Vertex_Index_1, int i_Vertex_Index_2);
-    bool checkEdge(int i_Vertex_Index_1, int i_Vertex_Index_2);
-    void PrintToFile(int size,std::string filename);
-    void printMatrix(int size, std::ostream& out = std::cout);
-    int vertexDegree(int idx);
-    int getVerticesSize() const;
     std::vector<int> getNeighbourIndices(int idx) const;
     std::vector<int> getOutNeighbourIndices(int idx) const;
+    void printMatrix(std::ostream& out = std::cout);
     void printNeighbourIndices(int idx);
     int getNumberOfEdges();
-    void readFromFile(std::string path,bool wagi);
-private:
-    void clear();
+    int getVerticesSize();
 };
