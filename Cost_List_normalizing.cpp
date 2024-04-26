9030393f5e5ef6a4d658dc47bd2c0cef8966f7a8
jansaklak <101001672+jansaklak@users.noreply.github.com>
2024-04-26 17:03:03 +0200

    Add files via upload

--- Cost_List_normalizing.cpp
+++ Cost_List_normalizing.cpp
@@ -24,6 +24,7 @@ struct WeightTable {
     double reCalc;
     double idleTime;
     double asBefore;
+    double SUM;
 };
 
 void printWeightTable(const WeightTable& wt) {
@@ -40,6 +41,90 @@ void printWeightTable(const WeightTable& wt) {
     std::cout << "idleTime: " << std::setw(5) << wt.idleTime << "|";
     std::cout << "asBefore: " << std::setw(5) << wt.asBefore << "\n";
 }
+
+void normalize(std::vector<WeightTable>& values) {
+    if (values.empty()) return;
+
+    double min_TCP = std::numeric_limits<double>::max();
+    double max_TCP = std::numeric_limits<double>::min();
+
+    double min_TC = std::numeric_limits<double>::max();
+    double max_TC = std::numeric_limits<double>::min();
+
+    double min_Tw = std::numeric_limits<double>::max();
+    double max_Tw = std::numeric_limits<double>::min();
+
+    double min_ReTw = std::numeric_limits<double>::max();
+    double max_ReTw = std::numeric_limits<double>::min();
+
+    double min_Cw = std::numeric_limits<double>::max();
+    double max_Cw = std::numeric_limits<double>::min();
+
+    double min_ReCw = std::numeric_limits<double>::max();
+    double max_ReCw = std::numeric_limits<double>::min();
+
+    double min_tasksOnInst = std::numeric_limits<double>::max();
+    double max_tasksOnInst = std::numeric_limits<double>::min();
+
+    double min_runTime = std::numeric_limits<double>::max();
+    double max_runTime = std::numeric_limits<double>::min();
+
+    double min_reCalc = std::numeric_limits<double>::max();
+    double max_reCalc = std::numeric_limits<double>::min();
+
+    double min_idleTime = std::numeric_limits<double>::max();
+    double max_idleTime = std::numeric_limits<double>::min();
+
+    for (const auto& wt : values) {
+        if (wt.TCP < min_TCP) min_TCP = wt.TCP;
+        if (wt.TCP > max_TCP) max_TCP = wt.TCP;
+
+        if (wt.TC < min_TC) min_TC = wt.TC;
+        if (wt.TC > max_TC) max_TC = wt.TC;
+
+        if (wt.Tw < min_TC) min_TC = wt.Tw;
+        if (wt.Tw > max_Tw) max_Tw = wt.Tw;
+
+        if (wt.reTw < min_ReTw) min_ReTw = wt.reTw;
+        if (wt.reTw > max_ReTw) max_ReTw = wt.reTw;
+
+        if (wt.Cw < min_Cw) min_Cw = wt.Cw;
+        if (wt.Cw > max_Cw) max_Cw = wt.Cw;
+
+        if (wt.reCw < min_ReCw) min_ReCw = wt.reCw;
+        if (wt.reCw > max_ReCw) max_ReCw = wt.reCw;
+
+        if (wt.StartingTime < min_tasksOnInst) min_tasksOnInst = wt.StartingTime;
+        if (wt.StartingTime > max_tasksOnInst) max_tasksOnInst = wt.StartingTime;
+
+        if (wt.runTime < min_runTime) min_runTime = wt.runTime;
+        if (wt.runTime > max_runTime) max_runTime = wt.runTime;
+
+        if (wt.reCalc < min_reCalc) min_reCalc = wt.reCalc;
+        if (wt.reCalc > max_reCalc) max_reCalc = wt.reCalc;
+
+        if (wt.idleTime < min_idleTime) min_idleTime = wt.idleTime;
+        if (wt.idleTime > max_idleTime) max_idleTime = wt.idleTime;
+    }
+
+    for (auto& wt : values) {
+        wt.TCP = (wt.TCP - min_TCP) / (max_TCP - min_TCP)                                           * 0.2;
+        wt.TC = (wt.TC - min_TC) / (max_TC - min_TC)                                                * 0.1;
+        wt.Tw = (wt.Tw - min_Tw) / (max_Tw - min_Tw)                                                * 0.025;
+        wt.reTw = (wt.reTw - min_ReTw) / (max_ReTw - min_ReTw)                                      * 0.025;
+        wt.Cw = (wt.Cw - min_Cw) / (max_Cw - min_Cw)                                                * 0.025;
+        wt.reCw = (wt.reCw - min_ReCw) / (max_ReCw - min_ReCw)                                      * 0.025;
+        wt.StartingTime = (wt.StartingTime - min_tasksOnInst) / (max_tasksOnInst - min_tasksOnInst) * 0.05;
+        wt.runTime = (wt.runTime - min_runTime) / (max_runTime - min_runTime)                       * 0.1;
+        wt.reCalc = (wt.reCalc - min_reCalc) / (max_reCalc - min_reCalc)                            * 0.3;
+        wt.idleTime = (wt.idleTime - min_idleTime) / (max_idleTime - min_idleTime)                  * 0.1;
+        wt.asBefore = wt.asBefore * 0.05;
+        wt.SUM = wt.TCP + wt.TC + wt.Tw + wt.reTw + wt.Cw + wt.StartingTime + wt.runTime + wt.reCalc + wt.idleTime + wt.asBefore;                                                                  
+    }
+    
+}
+
+
 
 
 void Cost_List::getCurrWeight(int task_id){
@@ -68,10 +153,12 @@ void Cost_List::getCurrWeight(int task_id){
                     reCalculate(task_id,inst),longestIdle(task_id,inst),asBefore(task_id,inst)}
         );
         weightsTable.push_back(wt);
-        
         printWeightTable(wt);
     }
-
+    normalize(weightsTable);
+    for(WeightTable w : weightsTable){
+        std::cout << *w.inst << "\t\tPunkty: " << w.SUM << "\n";
+    }
 
 }
 
@@ -106,12 +193,12 @@ double Cost_List::allocated_cost(int task_id,const Instance* inst){
 }
 
 double Cost_List::inst_starting(int task_id,const Instance* inst){
-    if(inst->isVirtual()) return -1;
+    if(inst->isVirtual()) return 0;
     return inst->getTaskSet().size();
 }
 
 double Cost_List::inst_time_running(int task_id,const Instance* inst){
-    if(inst->isVirtual()) return -1;
+    if(inst->isVirtual()) return 0;
     return getTimeRunning(inst);
 }
 
@@ -120,13 +207,13 @@ double Cost_List::reCalculate(int task_id,const Instance* inst){
 }
 
 double Cost_List::longestIdle(int task_id,const Instance* inst){
-    if(inst->isVirtual()) return -1;
+    if(inst->isVirtual()) return 0;
     return getIdleTime(inst,getStartingTime(task_id));
 }
 
 double Cost_List::asBefore(int task_id,const Instance* inst){
     if(inst->isVirtual()){
-        return -1;
+        return 0;
     }
     for(int i : TaskGraph.getInNeighbourIndices(task_id)){
         if(getInstance(i) == inst){
